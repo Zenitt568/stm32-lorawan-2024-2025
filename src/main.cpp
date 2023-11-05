c8bd89e7d9449aae689ab25ee1f46494c4aa62b8
Bartosz Owczarczuk <133026728+Bartek011@users.noreply.github.com>
2023-11-05 12:53:34 +0100

    Fixed led

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "bme280_sensor.h"
 #include "ADC.h"
 
+
 uint8_t requestMessage[1];
 uint8_t receivedMessage[MESSAGE_SIZE];
 
@@ -10,9 +11,29 @@ bool nextMessage = true;
 uint8_t interruptState = 0;
 uint32_t currentTime = 0;
 
+volatile int interruptLedCount = 0;
+
 //LoRa::ModuleType_t moduleType = MODULE_TYPE;
 DataRead_t sensorData;
 DataReceived_t receivedData;
+
+// Create instance of TIM2
+HardwareTimer *MyTim = new HardwareTimer(TIM2);
+
+void turnOffLed(void){
+  digitalWrite(LED_GREEN, false);
+}
+
+void updateLedState(void)
+{
+  digitalWrite(LED_GREEN, !digitalRead(LED_GREEN)); // Change state of LED
+  interruptLedCount++;
+  if (interruptLedCount >= 8){ //After 8th change -> turn off timer
+    MyTim->pause();
+    interruptLedCount = 0;
+    turnOffLed(); //In case of LED staying turned ON
+  }
+}
 
 void setup()
 {
@@ -24,6 +45,12 @@ void setup()
   BME280::DataInit_Bme280(&sensorData);  // Init BME280 sensor duplicate
   LoRa::DataInit_Lora(&receivedData);
 
+  pinMode(LED_GREEN, OUTPUT); // configure PA5(builtin LED) as OUTPUT
+
+  /*Init Timer*/
+  MyTim->setOverflow(30, HERTZ_FORMAT); // set interrupt interval ~33ms
+  MyTim->attachInterrupt(updateLedState);
+
 #if SW_TYPE == MASTER
     attachInterrupt(digitalPinToInterrupt(BOARD_BTN), ButtonClickInterrupt, RISING);
 
@@ -33,7 +60,6 @@ void setup()
    ADC_Init();
 
 #endif // SW_TYPE == SLAVE
-
 }
 
 void loop()
@@ -43,6 +69,7 @@ void loop()
   #if SW_TYPE == MASTER
     if (interruptState)
     {
+      MyTim->resume(); // Start TIM2 and blinking
       LoRa::SendRequest();    // Should communication be done here or in lora.cpp?
       BOOL(interruptState);
     }
@@ -56,6 +83,7 @@ void loop()
   #elif SW_TYPE == SLAVE
     if (loraRadio.read(requestMessage) == 0x04)
     {
+      MyTim->resume(); // Start TIM2 and blinking
       memset(requestMessage, 0, 1);
       Serial.println("[INFO] Data request received");
 
@@ -70,3 +98,5 @@ void ButtonClickInterrupt(void)
 {
   BOOL(interruptState);
 }
+
+
