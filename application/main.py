544fa22ed65c6a2e89e65fbd6be20c61f091a108
Michprac <michal.poczta135@gmail.com>
2023-11-23 20:48:06 +0100

    Features: selecting ports, labels added

--- application/main.py
+++ application/main.py
@@ -6,8 +6,9 @@ from tkinter.messagebox import showinfo
 from serial.tools import list_ports
 import serial
 
-ports = list_ports()
-serial_object = serial.Serial()
+ports = list_ports.comports()
+serial_object_MASTER = serial.Serial()
+serial_object_SLAVE = serial.Serial()
 all_ports_str = ['Port not selected']
 element_to_combox = ''
 
@@ -25,11 +26,42 @@ for element in ports:
     all_ports_str.append(element_to_combox)
 
 
+def configure_serial(event,serial_object, box):
+    serial_object.baudrate = 115200
+    try:
+        serial_object.port = box.get()
+        serial_object.open()
+    except serial.serialutil.SerialException:
+        showerror(title="Error", message='Could not open port "' + box.get() + '"')
+        return
+
 
 window = tk.Tk()
 
-height = 900
-width = 900
+height = 500
+width = 680
 d_x = 500
-d_y = 0
+d_y = 100
 
+window.title('STM32 LoRa Communication')
+window.geometry(f"{height}x{width}+{d_x}+{d_y}")
+window.minsize(width, height)
+window.maxsize(width, height)
+
+tk.Label(window, text="MASTER",font=("Arial", 40)).grid(row=0, column = 0, stick='e', padx=10)
+tk.Label(window, text="SLAVE", font=("Arial", 40)).grid(row=0, column = 1, stick='e', padx=240)
+
+ports_box_master = ttk.Combobox(window, values=all_ports_str)
+ports_box_master.config(state="readonly")
+ports_box_master.bind("<<ComboboxSelected>>", lambda event: configure_serial(event, serial_object_MASTER, ports_box_master))
+ports_box_master.current(0)
+ports_box_master.grid(row=1, column=0)
+
+ports_box_slave = ttk.Combobox(window, values=all_ports_str)
+ports_box_slave.config(state="readonly")
+ports_box_slave.bind("<<ComboboxSelected>>", lambda event: configure_serial(event, serial_object_SLAVE, ports_box_slave))
+ports_box_slave.current(0)
+ports_box_slave.grid(row=1, column=1)
+
+
+window.mainloop()
