2b3e169d888d9fbb09c0be9003eae660f834dbc8
Michprac <michal.poczta135@gmail.com>
2023-12-04 21:29:10 +0100

    Added widgets for master and slave

--- application/main.py
+++ application/main.py
@@ -12,7 +12,9 @@ from tkinter.messagebox import showerror
 # --------------------------FUNCTIONS---------------------------
 
 # -------------------------------------POPUP------------------------------------------
-master_port, slave_port = '', ''
+master_port, slave_port = 'COM3', 'COM4'
+helpflag = True
+
 
 def close_window():
     global master_port, slave_port
@@ -23,7 +25,6 @@ def close_window():
     else:
         master_port = ports_box_master.get()
         slave_port = ports_box_slave.get()
-        print(master_port)
         pop.destroy()
 
 
@@ -76,28 +77,76 @@ def receive_data():
     print(master_port, slave_port)
 
 
-# def configure_serial(event):
-#     serial_object_MASTER.baudrate = 115200
-#     try:
-#         serial_object_MASTER.port = ports_box_master.get()
-#         serial_object_MASTER.open()
-#     except serial.serialutil.SerialException:
-#         showerror(title="Error", message='Could not open port "' + ports_box_master.get() + '"')
-#         return
+def configure_serial(event):
+    serial_object_MASTER.baudrate = 115200
+    try:
+        serial_object_MASTER.port = ports_box_master.get()
+        serial_object_MASTER.open()
+    except serial.serialutil.SerialException:
+        showerror(title="Error", message='Could not open port "' + ports_box_master.get() + '"')
+        return
 
 
-# def receive_data():
-#     if serial_object_MASTER.isOpen:
-#         while True:
-#             size = serial_object_MASTER.inWaiting()
-#             print(size)
-#             if size:
-#
-#                 data = str(serial_object_MASTER.read(size)) + '\n'
-#                 print(data)
-#                 master_text.insert(tk.END, data)
-#                 break
-#             time.sleep(1)
+def receive_data():
+
+    data_to_show_master = []
+    data_to_show_slave = []
+
+    if master_port and slave_port:
+        global serial_object_MASTER, serial_object_SLAVE
+
+        serial_object_MASTER = serial.Serial()
+        serial_object_SLAVE = serial.Serial()
+
+        serial_object_MASTER.baudrate = 115200
+        serial_object_SLAVE.baudrate = 115200
+        try:
+            serial_object_MASTER.port = master_port
+            serial_object_MASTER.open()
+
+        except serial.serialutil.SerialException:
+            showerror(title="Error", message='Could not open port "' + master_port + '"')
+
+        try:
+            serial_object_SLAVE.port = slave_port
+            serial_object_SLAVE.open()
+        except serial.serialutil.SerialException:
+            showerror(title="Error", message='Could not open port "' + slave_port + '"')
+
+        print(serial_object_MASTER.isOpen())
+
+        if serial_object_MASTER.isOpen and serial_object_SLAVE.isOpen():
+            i = 0
+            while True:
+                size_m = serial_object_MASTER.inWaiting()
+                print(size_m)
+                if size_m:
+                    data = str(serial_object_MASTER.read(size_m).decode("utf-8"))
+                    data_to_show_master.append(data)
+                    print(data)
+                    i = 0
+
+                size_l = serial_object_SLAVE.inWaiting()
+                print(size_l)
+                if size_l:
+                    data = str(serial_object_MASTER.read(size_l).decode("utf-8"))
+                    data_to_show_slave.append(data)
+                    print(data)
+                    i = 0
+
+                i = i + 1
+                time.sleep(1)
+                if i > 10:
+                    break
+
+            for element in data_to_show_master:
+                master_text.insert(tk.END, element)
+            for element in data_to_show_slave:
+                slave_text.insert(tk.END, element)
+    else:
+        print("Port haven't been chosen")
+
+
 
 # -------------------------------------END_WINDOW------------------------------------------
 # -----------------------------------------------------------------------------------------
@@ -108,7 +157,7 @@ def receive_data():
 window = tk.Tk()
 
 height = 500
-width = 680
+width = 600
 d_x = 500
 d_y = 100
 
@@ -120,13 +169,46 @@ window.maxsize(width, height)
 # function for creating popup window with com ports selection
 popup(window)
 
-tk.Label(window, text="MASTER",font=("Arial", 40)).grid(row=0, column = 0, stick='e', padx=10)
-tk.Label(window, text="SLAVE", font=("Arial", 40)).grid(row=0, column = 1, stick='e', padx=240)
 
-master_text = tk.Text(window, height=5, width=30)
-master_text.grid(row=2, column=0, pady=5, padx=5)
+tk.Label(window, text="MASTER",font=("Arial", 40)).grid(row=0, column = 0, padx=30)
+tk.Label(window, text="SLAVE", font=("Arial", 40)).grid(row=0, column = 2, padx=50)
 
-display_master_btn = tk.Button(window, text="Received", command=receive_data)
-display_master_btn.grid(row=3, column=0, pady=5, padx=5)
+master_text = tk.Text(window, height=5, width=30, wrap="none")
+master_text.grid(row=2, column=0, pady=5, padx=5, stick = 'ew')
+
+ys = ttk.Scrollbar(window, orient='vertical', command=master_text.yview)
+ys.grid(row=2, column=1, stick = 'ns')
+
+xs = ttk.Scrollbar(window, orient='horizontal', command=master_text.xview)
+xs.grid(row=3, column=0, columnspan =2, stick = 'ew')
+
+master_text['yscrollcommand'] = ys.set
+master_text['xscrollcommand'] = xs.set
+
+
+
+slave_text = tk.Text(window, height=5, width=30, wrap="none")
+slave_text.grid(row=2, column=2, pady=5, padx=10, stick = 'ew')
+
+ys = ttk.Scrollbar(window, orient='vertical', command=master_text.yview)
+ys.grid(row=2, column=3, stick = 'ns')
+
+xs = ttk.Scrollbar(window, orient='horizontal', command=master_text.xview)
+xs.grid(row=3, column=2, padx = 10, columnspan =2, stick = 'ew')
+
+master_text['yscrollcommand'] = ys.set
+master_text['xscrollcommand'] = xs.set
+
+
+
+display_master_btn = tk.Button(window, text="LISTEN TO DATA", command=receive_data)
+display_master_btn.grid(row=4, column=0, columnspan = 4, pady=40, padx=30, stick='ew')
+
+
+
+
+
+
+
 
 window.mainloop()
