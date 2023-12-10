e97ad8276f42e68814045504019d4007ae611b42
Michprac <michal.poczta135@gmail.com>
2023-12-10 14:19:41 +0100

    Version of app 1.2: fixed bugs, added counter

--- application/main.py
+++ application/main.py
@@ -13,7 +13,8 @@ from tkinter.messagebox import showerror
 
 # -------------------------------------POPUP------------------------------------------
 master_port, slave_port = '', ''
-helpflag = True
+
+
 
 
 def close_window():
@@ -72,19 +73,19 @@ def popup(window):
 
 # -------------------------------------WINDOW------------------------------------------
 
-def receive_data():
-    print("show port")
-    print(master_port, slave_port)
+# def receive_data():
+#     print("show port")
+#     print(master_port, slave_port)
 
 
-def configure_serial(event):
-    serial_object_MASTER.baudrate = 115200
-    try:
-        serial_object_MASTER.port = ports_box_master.get()
-        serial_object_MASTER.open()
-    except serial.serialutil.SerialException:
-        showerror(title="Error", message='Could not open port "' + ports_box_master.get() + '"')
-        return
+# def configure_serial(event):
+#     serial_object_MASTER.baudrate = 115200
+#     try:
+#         serial_object_MASTER.port = ports_box_master.get()
+#         serial_object_MASTER.open()
+#     except serial.serialutil.SerialException:
+#         showerror(title="Error", message='Could not open port "' + ports_box_master.get() + '"')
+#         return
 
 
 def receive_data():
@@ -96,7 +97,7 @@ def receive_data():
     data_to_show_slave = []
 
     if master_port and slave_port:
-        global serial_object_MASTER, serial_object_SLAVE
+        global serial_object_MASTER, serial_object_SLAVE, finish_text
 
         serial_object_MASTER = serial.Serial()
         serial_object_SLAVE = serial.Serial()
@@ -108,17 +109,20 @@ def receive_data():
             serial_object_MASTER.open()
 
         except serial.serialutil.SerialException:
-            showerror(title="Error", message='Could not open port "' + master_port + '"')
+            showerror(title="Error", message='Could not open MASTER-port "' + master_port + '"')
 
         try:
             serial_object_SLAVE.port = slave_port
             serial_object_SLAVE.open()
         except serial.serialutil.SerialException:
-            showerror(title="Error", message='Could not open port "' + slave_port + '"')
+            showerror(title="Error", message='Could not open SLAVE-port "' + slave_port + '"')
 
-        print(serial_object_MASTER.isOpen())
+        print("Master otwarty?: " +master_port + "--"+ str(serial_object_MASTER.isOpen()))
+        print("Slave otwarty?: " +slave_port+ "--"+ str(serial_object_SLAVE.isOpen()))
 
         if serial_object_MASTER.isOpen and serial_object_SLAVE.isOpen():
+            master_text.insert('1.0', "")
+            slave_text.insert('1.0', "")
             i = 0
             while True:
                 size_m = serial_object_MASTER.inWaiting()
@@ -133,20 +137,32 @@ def receive_data():
                 print("_______________________________________")
                 print(size_l)
                 if size_l:
-                    data_l = str(serial_object_SLAVE.read(size_l))
+                    data_l = str(serial_object_SLAVE.read(size_l).decode("utf-8"))
                     data_to_show_slave.append(data_l)
                     print(data_l)
                     i = 0
 
                 i = i + 1
+                finish_text.set("Loading will be finished in ..." + str(11 - i))
+
                 time.sleep(1)
                 if i > 10:
                     break
+                window.update()
 
-            for element in data_to_show_master:
-                master_text.insert(tk.END, element)
-            for element in data_to_show_slave:
-                slave_text.insert(tk.END, element)
+            if data_to_show_master == []:
+                master_text.insert(tk.END, "Nothing for MASTER!")
+            else:
+                for element in data_to_show_master:
+                    master_text.insert(tk.END, element)
+
+            if data_to_show_slave == []:
+                slave_text.insert(tk.END, "Nothing for SLAVE!")
+            else:
+                for element in data_to_show_slave:
+                    slave_text.insert(tk.END, element)
+
+            finish_text.set('')
     else:
         print("Port haven't been chosen")
 
@@ -160,10 +176,13 @@ def receive_data():
 
 window = tk.Tk()
 
-height = 280
+height = 480 #280
 width = 600
 d_x = 500
 d_y = 100
+
+#variable to display ""
+finish_text = tk.StringVar()
 
 window.title('STM32 LoRa Communication')
 window.geometry(f"{height}x{width}+{d_x}+{d_y}")
@@ -174,39 +193,40 @@ window.maxsize(width, height)
 popup(window)
 
 
-tk.Label(window, text="MASTER",font=("Arial", 40)).grid(row=0, column = 0, padx=30)
+tk.Label(window, text="MASTER", font=("Arial", 40)).grid(row=0, column = 0, padx=30)
 tk.Label(window, text="SLAVE", font=("Arial", 40)).grid(row=0, column = 2, padx=50)
 
 master_text = tk.Text(window, height=5, width=30, wrap="none")
-master_text.grid(row=2, column=0, pady=5, padx=5, stick = 'ew')
+master_text.grid(row=2, column=0, pady=5, padx=5, stick='ew')
 
-ys = ttk.Scrollbar(window, orient='vertical', command=master_text.yview)
-ys.grid(row=2, column=1, stick = 'ns')
+ys_m = ttk.Scrollbar(window, orient='vertical', command=master_text.yview)
+ys_m.grid(row=2, column=1, stick='ns')
 
-xs = ttk.Scrollbar(window, orient='horizontal', command=master_text.xview)
-xs.grid(row=3, column=0, columnspan =2, stick = 'ew')
+xs_m = ttk.Scrollbar(window, orient='horizontal', command=master_text.xview)
+xs_m.grid(row=3, column=0, columnspan=2, stick='ew')
 
-master_text['yscrollcommand'] = ys.set
-master_text['xscrollcommand'] = xs.set
+master_text['yscrollcommand'] = ys_m.set
+master_text['xscrollcommand'] = xs_m.set
 
 
 
 slave_text = tk.Text(window, height=5, width=30, wrap="none")
-slave_text.grid(row=2, column=2, pady=5, padx=10, stick = 'ew')
+slave_text.grid(row=2, column=2, pady=5, padx=10, stick='ew')
 
-ys = ttk.Scrollbar(window, orient='vertical', command=master_text.yview)
-ys.grid(row=2, column=3, stick = 'ns')
+ys_s = ttk.Scrollbar(window, orient='vertical', command=slave_text.yview)
+ys_s.grid(row=2, column=3, stick='ns')
 
-xs = ttk.Scrollbar(window, orient='horizontal', command=master_text.xview)
-xs.grid(row=3, column=2, padx = 10, columnspan =2, stick = 'ew')
+xs_s = ttk.Scrollbar(window, orient='horizontal', command=slave_text.xview)
+xs_s.grid(row=3, column=2, padx=10, columnspan=2, stick='ew')
 
-master_text['yscrollcommand'] = ys.set
-master_text['xscrollcommand'] = xs.set
+slave_text['yscrollcommand'] = ys_s.set
+slave_text['xscrollcommand'] = xs_s.set
 
 
+tk.Label(window, font="Arial", textvariable=finish_text).grid(row=4, column=0, columnspan=4, pady=10)
 
-display_master_btn = tk.Button(window, text="LISTEN TO DATA", command=receive_data)
-display_master_btn.grid(row=4, column=0, columnspan = 4, pady=40, padx=30, stick='ew')
+display_master_btn = tk.Button(window, text="LISTEN TO DATA", font="Arial", command=receive_data)
+display_master_btn.grid(row=5, column=0, columnspan=4, pady=5, padx=30, stick='ew')
 
 
 
